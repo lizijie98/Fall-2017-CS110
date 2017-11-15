a2b5c96f3180e66da69f3df9a03a6e2dbbb2e898
hroeder1 <hroeder1@binghamton.edu>
2017-11-15 13:27:04 -0500

    Basic Ship Physics

--- ship.py
+++ ship.py
@@ -1,16 +1,20 @@
+import gameobject
+import math
 class Ship:
 	def __init__(self, position, power, fuel, image):
-		self.x, self.y = position
-		self.direction = 0
+		self.object = gameobject.GameObject(position, 0, "Player")
+		self.velocity = (0,0)
 		self.power = power
 		self.fuel = fuel
 		self.image = image
 	def forward(self):
 		#lower fuel
+		self.fuel -= 1
 		#propel forward in current direction
+		self.velocity = (self.velocity[0] + self.power * math.cos(self.object.rotation), self.velocity[1] + self.power * math.sin(self.object.rotation))
 	def turn(self, angle):
-		self.direction += angle
-	def moveTowards(self, point, force):
-		#do math to figure this out
+		self.object.rotation += angle
+	def accelerateDown(self, force):
+		self.velocity = (self.velocity[0],self.velocity[1] - force)
 	
 		
--- tests.py
+++ tests.py
@@ -1,6 +1,15 @@
 import planet
+import ship
 def main():
-	plan = planet.Planet(10, 100, "Bleh")
-	print(plan.nodes)
-	plan.drawPlanet()
+	#plan = planet.Planet(10, 100)
+	#print(plan.nodes)
+	#plan.drawPlanet()
+
+	player = ship.Ship((0,0), 10, 500, "image")
+	player.forward()
+	print(player.velocity)
+	player.turn(90)
+	print(player.object.rotation)
+	player.accelerateDown(10)
+	print(player.velocity)
 main()
