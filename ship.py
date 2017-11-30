c42de429e952ad05caa631262d8972a293d4268e
Jack Li <33259323+lizijie98@users.noreply.github.com>
2017-11-30 14:52:01 -0500

    Add files via upload

--- ship.py
+++ ship.py
@@ -5,10 +5,9 @@ import math
 class Ship:
 	def __init__(self, position = (0,0)):
 		self.object = gameobject.GameObject(position, 0, "Player", "player")
-		self.Vertvelo = 0
+		self.velocity = (0,0)
 		#trying to make the vertical velocity drop by the gravitational accel every second
-		pygame.time.set_timer(self.Vertvelo - planet.gravity, 1) #not sure how the set_timer thing works.
-		self.Horivelo = 0
+		#pygame.time.set_timer(self.Vertvelo - planet.gravity, 1) #not sure how the set_timer thing works.
 		#two values below are actual values from apollo landing modules, might change to balance gameplay
 		self.mainPower = 4.3584
 		self.auxPower = 0.2748
@@ -17,24 +16,23 @@ class Ship:
 		self.auxFuel = 290
 		self.image = "SillySteve.jpeg"
 	def forward(self):
-		#lower fuel
 		print("Forward")
+		#Lower mainFuel
 		self.mainFuel -= 1
 		#propel forward in current direction
-		self.Vertvelo = self.Vertvelo + (self.mainPower * math.sin(self.object.rotation))
-		self.Horivelo = self.Horivelo + (self.mainPower * math.cos(self.object.rotation))
+		self.velocity = (self.velocity[0] + self.MainPower * math.cos(self.object.rotation), self.velocity[1] + self.MainPower * math.sin(self.object.rotation))
 	def left(self):
                 print("Left")
                 #lower auxFuel
                 self.auxFuel -= 1
                 #Move left
-                self.Horivelo -= self.auxPower
+                self.velocity = (self.velocity[0] - self.auxPower, self.velocity[1])
         def right(self):
                 print("Right")
                 #lower auxFuel
                 self.auxFuel -= 1
                 #Move left
-                self.Horivelo += self.auxPower
+                self.velocity = (self.velocity[0] + self.auxPower, self.velocity[1])
 	def turn(self, angle):
 		print("Turn " + str(angle))
 		self.object.rotation += angle	
